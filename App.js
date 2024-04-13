270ea135fa35a0e3b024f7458b1ce0096ba75df2
Eswar Saladi <hello@generalist.dev>
2024-04-13 16:16:04 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [fontsLoaded, fontError] = useFonts({
+		"Poppins-Regular": require("./assets/fonts/Poppins/Poppins-Regular.ttf"),
+	});
+
+	if (!fontsLoaded && !fontError) {
+		return null;
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text style={{ fontFamily: "Poppins-Regular", fontSize: 30 }}>
+				Money Vibes Less's Go
+			</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
